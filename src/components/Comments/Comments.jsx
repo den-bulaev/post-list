a9fb20e567d6ba8d41bdc809ae368009f6e75657
Bulaev Denis <bulaevdenis7@gmail.com>
2021-06-25 15:36:48 +0300

    plug for comments added

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Comments = ({ comments }) => (
-  <>
-    <h3>Comments:</h3>
-    <ul className="comments">
-      {comments.map(({ id, body }) => (
-        <li key={id}>
-          <p>{body}</p>
-        </li>
-      ))}
-    </ul>
-  </>
+  comments.length > 0 ? (
+    <>
+      <h3>Comments:</h3>
+      <ul className="comments">
+        {comments.map(({ id, body }) => (
+          <li key={id}>
+            <p>{body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <h3>There are no comments yet</h3>
+  )
 );
 
 Comments.propTypes = {
